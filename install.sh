e62e476a5ff6afca4d451fc19753d3d99b7111d4
Bansook Nam <bs.nam@navercorp.com>
2019-03-19 23:15:41 +0900

    remove specific version declare

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@
 
 echo "Installing template files..."
 
-find /Applications -path "*.app" -prune \( -name "*Android Studio 3.3*" -or -name "*IntelliJ IDEA*" \) -print0 | while read -d $'\0' folder
+find /Applications -path "*.app" -prune \( -name "*Android Studio*" -or -name "*IntelliJ IDEA*" \) -print0 | while read -d $'\0' folder
 do
   echo "\nInstalling to $folder"
   cp -frv $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/SVC* "$folder/Contents/plugins/android/lib/templates/activities/"
